ad4ebc3a9fd7fd89c292b7742626c072a119495c
Alexander Yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2025-06-20 13:15:50 +0200

    feat: iteration back into param setable

--- src/wq_insert_exec.c
+++ src/wq_insert_exec.c
@@ -19,12 +19,6 @@ MODULE_DESCRIPTION("My kernel module");
 MODULE_AUTHOR("yanovskyy");
 MODULE_LICENSE("GPL");
 
-#ifdef SET_ITERATION
-#define ITERATION SET_ITERATION
-#else
-#define ITERATION 1000
-#endif
-
 static int high_affinity = 0;
 module_param(high_affinity, int, 0644);
 MODULE_PARM_DESC(high_affinity, "0 for low affinity, 1 for high affinity");
@@ -33,6 +27,9 @@ static int unbound_or_bounded = 0;
 module_param(unbound_or_bounded, int, 0644);
 MODULE_PARM_DESC(unbound_or_bounded, "0 for unbound workqueue, 1 for bounded workqueue");
 
+static int iteration = 1000;
+module_param(iteration, int, 0644);
+MODULE_PARM_DESC(iteration, "Number of iterations for work insertion");
 
 #define PATH_MEASUREMENT_START "/tmp/wq-insert-exec-%d-%s-%s_affinity-%s.start"
 #define PATH_MEASUREMENT_END "/tmp/wq-insert-exec-%d-%s-%s_affinity-%s.end"
@@ -40,10 +37,16 @@ MODULE_PARM_DESC(unbound_or_bounded, "0 for unbound workqueue, 1 for bounded wor
 struct file *measurement_start_file = NULL;
 struct file *measurement_end_file = NULL;
 
-unsigned long long measurement_start_arr[ITERATION];
-unsigned long long measurement_end_arr[ITERATION];
+// unsigned long long measurement_start_arr[ITERATION];
+// unsigned long long measurement_end_arr[ITERATION];
+
+unsigned long long *measurement_start_arr = NULL;
+unsigned long long *measurement_end_arr = NULL;
+
 int index_measurement_start = 0;
 int index_measurement_end = 0;
+
+struct work_struct *works = NULL;
 
 void work_handler(struct work_struct *work)
 {
@@ -63,7 +66,6 @@ void update_measurement_end(unsigned long long end_time)
         measurement_end_arr[index_measurement_end++] = end_time;
 }
 
-
 void write_measurements_to_file(struct file *file, unsigned long long *arr, int count)
 {
     if (file && arr && count > 0)
@@ -77,14 +79,36 @@ void write_measurements_to_file(struct file *file, unsigned long long *arr, int 
     }
 }
 
-struct work_struct works[ITERATION];
-
 char path_start[512] = {0};
 char path_end[512] = {0};
 static int __init start(void)
 {
+    works = kmalloc_array(iteration, sizeof(struct work_struct), GFP_KERNEL);
+    if (unlikely(!works))
+    {
+        pr_err("Failed to allocate memory for works\n");
+        return -ENOMEM;
+    }
+
+    measurement_start_arr = kmalloc_array(iteration, sizeof(unsigned long long), GFP_KERNEL);
+    if (unlikely(!measurement_start_arr))
+    {
+        pr_err("Failed to allocate memory for measurement_start_arr\n");
+        kfree(works);
+        return -ENOMEM;
+    }
+
+    measurement_end_arr = kmalloc_array(iteration, sizeof(unsigned long long), GFP_KERNEL);
+    if (unlikely(!measurement_end_arr))
+    {
+        pr_err("Failed to allocate memory for measurement_end_arr\n");
+        kfree(measurement_start_arr);
+        kfree(works);
+        return -ENOMEM;
+    }
+
     char *bound_str = unbound_or_bounded ? "unbound" : "bounded";
-    char *affinity_str =  high_affinity ? "high" : "low";
+    char *affinity_str = high_affinity ? "high" : "low";
 
     struct timespec64 ts;
     struct tm tm;
@@ -95,14 +119,11 @@ static int __init start(void)
     time64_to_tm(ts.tv_sec, 0, &tm);
 
     // Format: YYYY-MM-DDTHH:MM:SSZ
-    snprintf(iso_timestamp, sizeof(iso_timestamp),
-             "%04ld-%02d-%02dT%02d:%02d:%02dZ",
-             tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
-             tm.tm_hour, tm.tm_min, tm.tm_sec);
+    snprintf(iso_timestamp, sizeof(iso_timestamp), "%04ld-%02d-%02dT%02d:%02d:%02dZ", tm.tm_year + 1900, tm.tm_mon + 1,
+             tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
-
-    snprintf(path_start, sizeof(path_start), PATH_MEASUREMENT_START, ITERATION, bound_str, affinity_str, iso_timestamp);
-    snprintf(path_end, sizeof(path_end), PATH_MEASUREMENT_END, ITERATION, bound_str, affinity_str, iso_timestamp);
+    snprintf(path_start, sizeof(path_start), PATH_MEASUREMENT_START, iteration, bound_str, affinity_str, iso_timestamp);
+    snprintf(path_end, sizeof(path_end), PATH_MEASUREMENT_END, iteration, bound_str, affinity_str, iso_timestamp);
 
     measurement_start_file = filp_open(path_start, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (unlikely(IS_ERR(measurement_start_file)))
@@ -119,9 +140,7 @@ static int __init start(void)
         return -1;
     }
 
-
-
-    init_hook_measurement_workqueue_insert_exec(update_measurement_start, update_measurement_end);    
+    init_hook_measurement_workqueue_insert_exec(update_measurement_start, update_measurement_end);
     init_measurement_workqueue_id(work_handler);
 
     struct workqueue_struct *wq = alloc_workqueue(
@@ -135,38 +154,57 @@ static int __init start(void)
     pr_info("%s: Workqueue created with %s affinity and %s bound\n", THIS_MODULE->name, high_affinity ? "high" : "low",
             unbound_or_bounded ? "unbound" : "bounded");
 
-    pr_info("%s: Starting work insertion with %d iterations\n", THIS_MODULE->name, ITERATION);
-    for (int i = 0; i < ITERATION; i++)
+    pr_info("%s: Starting work insertion with %d iterations\n", THIS_MODULE->name, iteration);
+    for (int i = 0; i < iteration; i++)
     {
         INIT_WORK(&works[i], work_handler);
         queue_work(wq, &works[i]);
     }
 
-    pr_info("%s: %d works queued\n", THIS_MODULE->name, ITERATION);
+    pr_info("%s: %d works queued\n", THIS_MODULE->name, iteration);
     return 0;
 }
 
-static void __exit end(void) { 
-    pr_info("%s: Exiting module\n", THIS_MODULE->name); 
+static void __exit end(void)
+{
+    pr_info("%s: Exiting module\n", THIS_MODULE->name);
     init_hook_measurement_workqueue_insert_exec(NULL, NULL);
     init_measurement_workqueue_id(NULL);
 
     write_measurements_to_file(measurement_start_file, measurement_start_arr, index_measurement_start);
     write_measurements_to_file(measurement_end_file, measurement_end_arr, index_measurement_end);
 
-    if (measurement_start_file) {
+    if (measurement_start_file)
+    {
         filp_close(measurement_start_file, NULL);
         measurement_start_file = NULL;
     }
-    if (measurement_end_file) {
+    if (measurement_end_file)
+    {
         filp_close(measurement_end_file, NULL);
         measurement_end_file = NULL;
     }
 
-    pr_info("%s: Measurement done, available at\n%s\n %s\n", THIS_MODULE->name,
-                path_start, path_end);
+    if (works)
+    {
+        kfree(works);
+        works = NULL;
+    }
+
+    if (measurement_start_arr)
+    {
+        kfree(measurement_start_arr);
+        measurement_start_arr = NULL;
+    }
+
+    if (measurement_end_arr)
+    {
+        kfree(measurement_end_arr);
+        measurement_end_arr = NULL;
+    }
+
+    pr_info("%s: Measurement done, available at\n%s\n %s\n", THIS_MODULE->name, path_start, path_end);
 }
 
 module_init(start);
 module_exit(end);
-
